3a12ba4771f6c006e65ff38079098b3e4233b85f
unknown <arturpopov@labpc044.computing.dundee.ac.uk>
2016-02-22 15:45:27 +0000

    buildings place & Canvas Added (esc to exit txt)

--- Assets/Scripts/BuildingPlacer.cs
+++ Assets/Scripts/BuildingPlacer.cs
@@ -7,13 +7,14 @@ public class BuildingPlacer : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		GameObject.Find ("Esc Text").GetComponent<CanvasGroup> ().alpha = 1;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		//Set to false
 		validPlace = false;
-
+		//
 		Vector3 mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 		mousePos.z = 0;
 		this.transform.position = mousePos;
@@ -26,7 +27,16 @@ public class BuildingPlacer : MonoBehaviour {
 
 		}
 
-		if (Input.GetMouseButton(0) && validPlace)
-			Debug.Log (validPlace);
+		if (validPlace && Input.GetMouseButtonDown(0)) {
+			Destroy(GetComponent<Rigidbody2D>());
+			Destroy(this);
+
+		}
+	}
+
+	void OnDestroy()
+	{
+		GameObject.Find ("Esc Text").GetComponent<CanvasGroup> ().alpha = 0;
+
 	}
 }
