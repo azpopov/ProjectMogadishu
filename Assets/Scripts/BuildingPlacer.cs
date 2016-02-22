35ac2408be22e0a6570aae15d808b4e5449974e7
unknown <arturpopov@labpc044.computing.dundee.ac.uk>
2016-02-22 15:15:20 +0000

    building placer, water as invalid location

---
+++ Assets/Scripts/BuildingPlacer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildingPlacer : MonoBehaviour {
+
+	bool validPlace = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		validPlace = false;
+
+		Vector3 mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		mousePos.z = 0;
+		this.transform.position = mousePos;
+
+		Collider2D collider = GetComponent<Collider2D> ();
+
+		if( !collider.IsTouchingLayers(LayerMask.GetMask("Water")))
+		   {
+			validPlace = true;
+
+		}
+
+		if (Input.GetMouseButton(0) && validPlace)
+			Debug.Log (validPlace);
+	}
+}
