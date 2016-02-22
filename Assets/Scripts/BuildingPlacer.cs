15c094e770435e1a2ad58a38406ca33379c0db28
unknown <arturpopov@labpc044.computing.dundee.ac.uk>
2016-02-22 15:55:20 +0000

    comments

--- Assets/Scripts/BuildingPlacer.cs
+++ Assets/Scripts/BuildingPlacer.cs
@@ -4,30 +4,35 @@ using System.Collections;
 public class BuildingPlacer : MonoBehaviour {
 
 	bool validPlace = false;
-
+	Collider2D collider;
 	// Use this for initialization
 	void Start () {
+		//set UI aplha to visible
 		GameObject.Find ("Esc Text").GetComponent<CanvasGroup> ().alpha = 1;
+		//Get the Collider
+		collider = GetComponent<Collider2D> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		//Set to false
 		validPlace = false;
-		//
+		//Get current mouse coord
 		Vector3 mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		//Put the Z axis to 0 so not behind camera
 		mousePos.z = 0;
+		//Whatever this script is attached to, follow mouse.
 		this.transform.position = mousePos;
-
-		Collider2D collider = GetComponent<Collider2D> ();
-
+		//As long as it's not touching Water
 		if( !collider.IsTouchingLayers(LayerMask.GetMask("Water")))
 		   {
 			validPlace = true;
 
 		}
-
+		//If able to place & valid location
 		if (validPlace && Input.GetMouseButtonDown(0)) {
+			//Destroy components
+
 			Destroy(GetComponent<Rigidbody2D>());
 			Destroy(this);
 
