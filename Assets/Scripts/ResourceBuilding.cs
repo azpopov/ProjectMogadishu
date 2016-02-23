d9a83cd63f1b752431a333c84c9ac6a6cd18a377
unknown <arturpopov@labpc072.computing.dundee.ac.uk>
2016-02-23 17:49:55 +0000

    float text bugged

--- Assets/Scripts/ResourceBuilding.cs
+++ Assets/Scripts/ResourceBuilding.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class ResourceBuilding : MonoBehaviour {
@@ -10,9 +11,12 @@ public class ResourceBuilding : MonoBehaviour {
 	SpriteRenderer spriteRnd;
 	Sprite defaultSprite;
 	public Sprite glowSprite;
+	TextMesh floatText;
+	float floatTextLive = 0.0f; 
 	// Use this for initialization
 	void Start () {
 		spriteRnd = GetComponent<SpriteRenderer> ();
+		floatText = GetComponentInChildren<TextMesh> ();
 		if (spriteRnd != null)
 			defaultSprite = spriteRnd.sprite;
 	}
@@ -20,7 +24,8 @@ public class ResourceBuilding : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
-		timeSinceTick += Time.deltaTime;
+		float timeSinceLastUpdate = Time.deltaTime;
+		timeSinceTick += timeSinceLastUpdate;
 		if (timeSinceTick > 3) 
 		{
 			spriteRnd.sprite = glowSprite;
@@ -32,11 +37,27 @@ public class ResourceBuilding : MonoBehaviour {
 				storedResources += Random.Range(50,150);
 			timeSinceTick = 0.0f;
 		}
+		if (floatTextLive > float.Epsilon) 
+		{
+			floatTextLive -= timeSinceLastUpdate;
+			Vector3 newPos = new Vector3(0f, 0.00125f * timeSinceLastUpdate);
+			floatText.gameObject.transform.Translate(newPos);
+			floatText.color -= new Color(floatText.color.r, floatText.color.g, floatText.color.b, floatText.color.a - 127*timeSinceLastUpdate);
+		}
 	}
 
 	void OnMouseDown()
 	{
-		spriteRnd.sprite = defaultSprite;
+		if (floatText != null) 
+		{
+			floatText.text = storedResources.ToString();
+			floatText.color = new Color(floatText.color.r, floatText.color.g, floatText.color.b, 255);
+			floatText.transform.Translate(new Vector3(this.gameObject.transform.position.x, 0.5f));
+			floatTextLive = 2.0f;
+
+		}
+		if(spriteRnd != null)
+			spriteRnd.sprite = defaultSprite;
 		ResourceManager.current.commodities += storedResources;
 		storedResources = 0;
 	}
